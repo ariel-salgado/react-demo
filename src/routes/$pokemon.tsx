494f20a7c0375ff6150c2a2bf5d9ea466e520684
Ariel Salgado <ariel.salgado.acevedo@gmail.com>
2025-10-09 21:04:19 -0300

    feat: display entries and stats

--- src/routes/$pokemon.tsx
+++ src/routes/$pokemon.tsx
@@ -1,27 +1,35 @@
 import { createFileRoute } from '@tanstack/react-router'
 
 import { pokemonAPI } from '@/lib/api/pokemon'
-import { PokemonSpriteCard } from '@/lib/components/pokemon';
+import { PokemonFlavorTexts, PokemonSpriteCard, PokemonStatsRadar, PokemonStatsTable } from '@/lib/components/pokemon';
 
 export const Route = createFileRoute('/$pokemon')({
   component: RouteComponent,
   loader: async ({ params }) => {
     const pokemon = await pokemonAPI.getPokemon(params.pokemon);
+    const pokemonEntries = await pokemonAPI.getPokemonFlavorTexts(params.pokemon);
 
     if (!pokemon) {
       throw new Error('Pokémon not found')
     }
 
-    return { pokemon }
+    return { pokemon, pokemonEntries }
   },
 })
 
 function RouteComponent() {
-  const { pokemon } = Route.useLoaderData()
+  const { pokemon, pokemonEntries } = Route.useLoaderData()
 
   return (
-    <section className="p-6 flex flex-col items-center gap-6">
-      <PokemonSpriteCard pokemon={pokemon} />
-    </section>
+    <div className="max-w-6xl mx-auto space-y-8 py-8">
+      <div className="grid gap-8">
+        <div className='flex gap-8'>
+          <PokemonSpriteCard className='w-[45%]' pokemon={pokemon} />
+          <PokemonFlavorTexts className='w-[55%]' entries={pokemonEntries} />
+        </div>
+        <PokemonStatsRadar pokemon={pokemon} />
+        <PokemonStatsTable pokemon={pokemon} />
+      </div>
+    </div>
   )
 }
