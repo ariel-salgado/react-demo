2edf819c2115beb02963c2683c382e5fe5ac32c6
Ariel Salgado <ariel.salgado.acevedo@gmail.com>
2025-10-09 21:03:14 -0300

    feat: added PokemonFlavorTexts component

--- src/lib/components/pokemon/card.tsx
+++ src/lib/components/pokemon/card.tsx
@@ -1,9 +1,12 @@
-import type { Pokemon } from '@/lib/api/pokemon'
+import type { HTMLAttributes } from 'react'
+import type { FlavorTextEntry, Pokemon } from '@/lib/api/pokemon'
 
 import { cn } from '@/lib/utils'
 import { Play, RotateCcw, Sparkles } from 'lucide-react'
-import { useState, type HTMLAttributes } from 'react'
+import { useState } from 'react'
 import { Button } from '../ui'
+import { PokemonTypeBadge } from './badge'
+import { Link } from '@tanstack/react-router'
 
 interface PokemonInfoCardProps extends Omit<HTMLAttributes<HTMLDivElement>, 'children'> {
     pokemon: Pokemon
@@ -32,13 +35,14 @@ function PokemonInfoCard({ pokemon, ref, className, ...rest }: PokemonInfoCardPr
             </div>
 
             <div className='flex flex-col gap-y-0.5 justify-center items-center'>
-                <a
+                <Link
                     className="whitespace-nowrap font-semibold text-2xl capitalize text-background hover:underline focus-within:underline underline-offset-2 decoration-2 cursor-pointer"
                     referrerPolicy='no-referrer'
-                    href={`/${pokemon.name}`}
+                    to={'/$pokemon'}
+                    params={{ pokemon: pokemon.name }}
                 >
                     {pokemon.name}
-                </a>
+                </Link>
                 <p className="text-center text-lg text-muted font-medium">
                     #{pokemon.id}
                 </p>
@@ -51,7 +55,7 @@ interface PokemonSpriteCardProps extends Omit<HTMLAttributes<HTMLDivElement>, 'c
     pokemon: Pokemon
 }
 
-function PokemonSpriteCard({ pokemon, ...rest }: PokemonSpriteCardProps) {
+function PokemonSpriteCard({ pokemon, className, ...rest }: PokemonSpriteCardProps) {
     const [is_back, set_is_back] = useState(false)
     const [is_shiny, set_is_shiny] = useState(false)
     const [is_playing, set_is_playing] = useState(false)
@@ -63,7 +67,7 @@ function PokemonSpriteCard({ pokemon, ...rest }: PokemonSpriteCardProps) {
         if (is_playing) return
 
         const audio = new Audio(pokemon.cries.latest)
-        audio.volume = 0.15
+        audio.volume = 0.2
         set_is_playing(true)
 
         audio.play()
@@ -73,19 +77,29 @@ function PokemonSpriteCard({ pokemon, ...rest }: PokemonSpriteCardProps) {
 
 
     return (
-        <div className="p-8 bg-foreground rounded-2xl" {...rest}>
-            <hgroup>
-                <h2 className="text-3xl font-bold capitalize text-center text-background">{pokemon.name}</h2>
-                <p className='text-muted text-lg text-center font-medium'>#{pokemon.id}</p>
-            </hgroup>
-            <div className="size-80 aspect-square overflow-hidden">
-                <img
-                    src={sprite_url}
-                    alt={`${pokemon.name} sprite`}
-                    className="size-full object-contain"
-                />
+        <div className={cn("relative p-8 bg-foreground rounded-2xl w-128", className)} {...rest}>
+            <div className='flex flex-col justify-between gap-y-6 items-center'>
+                <hgroup>
+                    <h2 className="text-4xl font-bold capitalize text-center text-background">{pokemon.name}</h2>
+                    <p className='text-muted text-2xl text-center'>#{pokemon.id}</p>
+                </hgroup>
+
+                <div className="size-80 aspect-square overflow-hidden">
+                    <img
+                        src={sprite_url}
+                        alt={`${pokemon.name} sprite`}
+                        className="size-full object-contain"
+                    />
+                </div>
+
+                <div className='flex gap-x-2 items-center justify-center'>
+                    {pokemon.types.map(({ type }, key) => (
+                        <PokemonTypeBadge type={type.name} key={key} />
+                    ))}
+                </div>
             </div>
-            <div className="flex justify-center gap-x-2">
+
+            <div className=" absolute top-8 left-8 flex flex-col justify-center gap-2">
                 <Button
                     size='icon'
                     variant='primary'
@@ -116,7 +130,36 @@ function PokemonSpriteCard({ pokemon, ...rest }: PokemonSpriteCardProps) {
     )
 }
 
+interface PokemonFlavorTextsProps extends HTMLAttributes<HTMLDivElement> {
+    entries: FlavorTextEntry[]
+}
+
+export default function PokemonFlavorTexts({ entries, className, ...rest }: PokemonFlavorTextsProps) {
+    return (
+        <div className={cn("p-8 bg-foreground rounded-2xl space-y-6", className)} {...rest}>
+            <h2 className="text-2xl font-semibold text-background">Pokédex Entries ({entries.length})</h2>
+
+            <div className="max-h-96 overflow-y-auto space-y-6 pr-3">
+                {entries.map((entry, key) => (
+                    <div
+                        key={key}
+                        className='bg-secondary/10 border-2 border-secondary p-6 rounded-2xl'
+                    >
+                        <p className="text-lg font-semibold text-background mb-1">
+                            <span className="capitalize">#{key + 1} - Pokemon {entry.version}</span>
+                        </p>
+                        <p className="text-sm text-background font-medium italic leading-snug">
+                            “{entry.flavor_text}”
+                        </p>
+                    </div>
+                ))}
+            </div>
+        </div>
+    )
+}
+
 export {
     PokemonInfoCard,
     PokemonSpriteCard,
+    PokemonFlavorTexts
 }
