d54b33d066824519a792b116188ff6c972f4b883
Ariel Salgado <ariel.salgado.acevedo@gmail.com>
2025-10-09 21:04:06 -0300

    refactor: update exports

--- src/lib/components/pokemon/index.ts
+++ src/lib/components/pokemon/index.ts
@@ -1,6 +1,12 @@
-import { PokemonInfoCard, PokemonSpriteCard } from './card';
+import { PokemonTypeBadge } from './badge';
+import { PokemonStatsRadar, PokemonStatsTable } from './stats';
+import { PokemonInfoCard, PokemonSpriteCard, PokemonFlavorTexts } from './card';
 
 export {
+    PokemonStatsRadar,
     PokemonInfoCard,
-    PokemonSpriteCard
+    PokemonSpriteCard,
+    PokemonTypeBadge,
+    PokemonStatsTable,
+    PokemonFlavorTexts
 }
